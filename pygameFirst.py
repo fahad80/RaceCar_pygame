479918883408b518a44ed2527f69d5441c9630fc
fahadmirza <fahadmirza80@yahoo.com>
2016-10-10 03:02:17 -0500

    Moving tree and moving road added!

--- pygameFirst.py
+++ pygameFirst.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 import time
+import random
 
 
 pygame.init()
@@ -13,10 +14,15 @@ GREEN   = (0,255,0)
 
 # Load Images
 carImg  = pygame.image.load(os.path.join("image","Racecar2.png"))
-roadImg = pygame.image.load(os.path.join("image","Road.jpg"))
+roadImg = []
+roadImg.append(pygame.image.load(os.path.join("image","Road1.jpg")))
+roadImg.append(pygame.image.load(os.path.join("image","Road2.jpg")))
+treeImg = []
+treeImg.append(pygame.image.load(os.path.join("image","tree2.png")))
+treeImg.append(pygame.image.load(os.path.join("image","tree3.png")))
 
 (car_width, car_height) = carImg.get_rect().size
-(display_width, display_height) = roadImg.get_rect().size
+(display_width, display_height) = roadImg[0].get_rect().size
 
 # Initialize Display
 pygame.display.set_caption('RaceCar')
@@ -31,8 +37,11 @@ font = pygame.font.Font("freesansbold.ttf", 36)
 def drawCar(x,y):
     gameDisplay.blit(carImg,(x,y))
 
-def drawRoad(x,y):
-    gameDisplay.blit(roadImg,(x,y))
+def drawRoad(x,y, roadNum):
+    gameDisplay.blit(roadImg[roadNum],(x,y))
+
+def drawTree(x,y,treeNum):
+    gameDisplay.blit(treeImg[treeNum],(x,y))
 
 def displayMsgCenter(text):
     largeText = font.render(text, True, BLACK)
@@ -46,14 +55,24 @@ def displayMsgCenter(text):
 def gameLoop():
     carX = gameDisplay.get_rect().centerx - (int)(car_width/2)
     carY = (display_height - car_height - 10)
-    roadX = 0
-    roadY = 0
 
+    roadNum = 0
+    roadX   = 0
+    roadY   = 0
+    
+    treeNum = random.randrange(0, 100)%2
+    treeX   = 0
+    treeY   = 0 -(treeImg[treeNum].get_rect().size[1])
+   
+    
     x_change = 0
+    ticks=pygame.time.get_ticks() #starter tick
     
     while True:
         # Draw and Update
-        drawRoad(roadX, roadY)
+        
+        drawRoad(roadX, roadY, roadNum)
+        drawTree(treeX,treeY, treeNum)
         drawCar(carX, carY)
         pygame.display.update()
         clock.tick(30)
@@ -76,6 +95,17 @@ def gameLoop():
             displayMsgCenter("You Crashed!!!")
             carX = gameDisplay.get_rect().centerx - (int)(car_width/2)
             carY = (display_height - car_height - 10)
+            treeX   = 0
+            treeY   = 0 -(treeImg[treeNum].get_rect().size[1])
+
+        if(pygame.time.get_ticks() - ticks) > 100:
+            roadNum ^= 1
+            treeY += 7
+            if treeY > display_height:
+                treeY = 0 -(treeImg[treeNum].get_rect().size[1])
+                treeNum = random.randrange(0, 100)%2
+
+            ticks=pygame.time.get_ticks()
             
 
 
