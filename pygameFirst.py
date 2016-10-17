6cbd88853f6d54ee95b81e1780bbc2e08003641f
fahadmirza <fahadmirza80@yahoo.com>
2016-10-17 01:25:22 -0500

    Image class added. Init Image position func added.

--- pygameFirst.py
+++ pygameFirst.py
@@ -4,44 +4,38 @@ import time
 import random
 
 
-pygame.init()
 
-# Colors Constant
+
+### Colors Constant
 BLACK   = (0,0,0)
 WHITE   = (255,255,255)
 RED     = (255,0,0)
 GREEN   = (0,255,0)
 
-# Load Images
-carImg  = pygame.image.load(os.path.join("image","Racecar2.png"))
-roadImg = []
-roadImg.append(pygame.image.load(os.path.join("image","Road1.jpg")))
-roadImg.append(pygame.image.load(os.path.join("image","Road2.jpg")))
-treeImg = []
-treeImg.append(pygame.image.load(os.path.join("image","tree2.png")))
-treeImg.append(pygame.image.load(os.path.join("image","tree3.png")))
-
-(car_width, car_height) = carImg.get_rect().size
-(display_width, display_height) = roadImg[0].get_rect().size
-
-# Initialize Display
-pygame.display.set_caption('RaceCar')
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-clock = pygame.time.Clock()
-
-# Initialize Font
-font = pygame.font.Font("freesansbold.ttf", 36)
 
 
-# Function definition
-def drawCar(x,y):
-    gameDisplay.blit(carImg,(x,y))
+### Class and Function definitions
 
-def drawRoad(x,y, roadNum):
-    gameDisplay.blit(roadImg[roadNum],(x,y))
+class gameImage:
+    X = 0
+    Y = 0
+    
+    def __init__(self, path):
+        self.img = pygame.image.load(path)
+        (self.width, self.height) = self.img.get_rect().size
+    
+    def draw(self,x,y):
+        gameDisplay.blit(self.img,(x,y))
 
-def drawTree(x,y,treeNum):
-    gameDisplay.blit(treeImg[treeNum],(x,y))
+
+def initImagePos():
+    carImg.X = gameDisplay.get_rect().centerx - (int)(carImg.width/2)
+    carImg.Y = (display_height - carImg.height - 10)
+
+    for tree in treeImg:
+        tree.X  = 0
+        tree.Y  = 0 -(tree.height)
+    
 
 def displayMsgCenter(text):
     largeText = font.render(text, True, BLACK)
@@ -53,34 +47,29 @@ def displayMsgCenter(text):
     
 
 def gameLoop():
-    carX = gameDisplay.get_rect().centerx - (int)(car_width/2)
-    carY = (display_height - car_height - 10)
-
-    roadNum = 0
-    roadX   = 0
-    roadY   = 0
     
+    gameExit = False
+    roadNum = 0
     treeNum = random.randrange(0, 100)%2
-    treeX   = 0
-    treeY   = 0 -(treeImg[treeNum].get_rect().size[1])
-   
+
+    initImagePos()   
     
     x_change = 0
     ticks=pygame.time.get_ticks() #starter tick
     
-    while True:
-        # Draw and Update
+    while gameExit != True:
+        ### Draw and Update
         
-        drawRoad(roadX, roadY, roadNum)
-        drawTree(treeX,treeY, treeNum)
-        drawCar(carX, carY)
+        roadImg[roadNum].draw(roadImg[roadNum].X, roadImg[roadNum].Y)
+        treeImg[treeNum].draw(treeImg[treeNum].X, treeImg[treeNum].Y)
+        carImg.draw(carImg.X, carImg.Y)
         pygame.display.update()
         clock.tick(30)
     
-        # Event Loop
+        ### Event Loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                gameExit = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
@@ -90,24 +79,42 @@ def gameLoop():
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
 
-        carX += x_change
-        if (carX > display_width - car_width) or (carX < 0):
+        carImg.X += x_change
+        if (carImg.X > display_width - carImg.width) or (carImg.X < 0):
             displayMsgCenter("You Crashed!!!")
-            carX = gameDisplay.get_rect().centerx - (int)(car_width/2)
-            carY = (display_height - car_height - 10)
-            treeX   = 0
-            treeY   = 0 -(treeImg[treeNum].get_rect().size[1])
+            initImagePos()
 
         if(pygame.time.get_ticks() - ticks) > 100:
             roadNum ^= 1
-            treeY += 7
-            if treeY > display_height:
-                treeY = 0 -(treeImg[treeNum].get_rect().size[1])
-                treeNum = random.randrange(0, 100)%2
+            treeImg[treeNum].Y += 7
+            if treeImg[treeNum].Y > display_height:
+                treeImg[treeNum].Y = 0 -(treeImg[treeNum].height)
+                treeNum = random.randrange(0, 2)%2
 
             ticks=pygame.time.get_ticks()
             
 
+pygame.init()
+### Load Images
+carImg = gameImage(os.path.join("image","Racecar2.png"))
+roadImg = []
+roadImg.append(gameImage(os.path.join("image","Road1.jpg")))
+roadImg.append(gameImage(os.path.join("image","Road2.jpg")))
+treeImg = []
+treeImg.append(gameImage(os.path.join("image","tree2.png")))
+treeImg.append(gameImage(os.path.join("image","tree3.png")))
+
+display_width  = roadImg[0].width
+display_height = roadImg[0].height
+
+### Initialize Display
+pygame.display.set_caption('CarDriving')
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+clock = pygame.time.Clock()
+
+### Initialize Font
+font = pygame.font.Font("freesansbold.ttf", 36)
 
 gameLoop()
+pygame.quit()
 #quit()
